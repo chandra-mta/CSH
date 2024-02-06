0fe01c97266fc0fdc5f47463304cfcb0835d0226
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-02-06 14:31:38 -0500

    correct formatting for testing script

--- CSH/test_plots/test/test_plot_msid_latest_conda.py
+++ CSH/test_plots/test/test_plot_msid_latest_conda.py
@@ -1,12 +1,13 @@
 #!/proj/sot/ska3/flight/bin/python
 
-import sys,os
+import sys
+import os
 
 #Path altering to import script which is undergoing testing
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.dirname(TEST_DIR)
 OUT_DIR = f"{TEST_DIR}/outTest"
-sys.path.insert(0,PARENT_DIR)
+sys.path.insert(0, PARENT_DIR)
 LIVE_BIN_DIR = "/data/mta4/www/CSH/test_plots"
 
 NEW_DIR_LIST = [OUT_DIR]
@@ -14,7 +15,7 @@ for dir in NEW_DIR_LIST:
 	os.system(f"mkdir -p {dir}")
 
 #Extra Parameter Setup
-#TEST_MSID_GROUP_SELECTION = ['Sys_temps', 'Spcelec']
+TEST_MSID_GROUP_SELECTION = ['Sys_temps', 'Spcelec']
 
 import plot_msid_latest_conda as pml
 import soh_msid_plot_class_v3 as smpc
@@ -28,7 +29,4 @@ for mod in MOD_GROUP:
     if hasattr(mod,'OUT_DIR'):
 	    mod.OUT_DIR = f"{OUT_DIR}"
 
-if 'TEST_MSID_GROUP_SELECTION' in locals():
-	pml.gen_plots(TEST_MSID_GROUP_SELECTION)
-else:
-	pml.gen_plots()
+pml.gen_plots(TEST_MSID_GROUP_SELECTION)
