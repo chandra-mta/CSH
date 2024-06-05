41a028d8ab2c0fa05a583d4db51730cb62ed1240
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2024-06-05 13:08:52 -0400

    testing methods for next_comm_check

--- next_comm_check.py
+++ next_comm_check.py
@@ -9,9 +9,10 @@
 #       last update: Mar 15, 2021                                                       #
 #                                                                                       #
 #########################################################################################
-import re
-import time
-import Chandra.Time
+import os
+from datetime import datetime
+from cxotime import CxoTime
+import argparse
 
 
 HOUSE_KEEPING = '/data/mta4/Script/SOH/house_keeping'
@@ -29,29 +30,32 @@ def find_next_comm():
     output: {HTML_DIR}/ctest.xml
             {HTML_DIR}/ncomm
     """
-    out = time.strftime('%Y:%j:%H:%M:%S', time.gmtime())
-    ctime = Chandra.Time.DateTime(out).secs
+    ctime = CxoTime(datetime.utcnow()).secs
     
     with open(f"{HTML_DIR}/comm_list.html") as f:
         data = [line.strip() for line in f.readlines()]
     
     pstop = 0.0
     for ent in data[11:]:
-        atemp = re.split('<td>', ent)
+        atemp = ent.split('<td>')
         start = atemp[1].replace('</td>','')
-        start = Chandra.Time.DateTime(start).secs
+        start = CxoTime(start).secs
         stop  = atemp[3].replace('</td></tr>','')
-        stop  = Chandra.Time.DateTime(stop).secs
+        stop  = CxoTime(stop).secs
     
         if (ctime > pstop) and (ctime < start):
             diff = start - ctime
-            ltime = 'Next Comm In: '   + convert_to_hour(diff)
+            hour, remainder = divmod(int(diff),3600)
+            minute = minute = remainder//60
+            ltime = f"Next Comm In:  {hour:>02}:{minute:>02}"
             with open(f"{HOUSE_KEEPING}/stime_to_comm", 'w') as fo:
-                fo.write(f"{diff}\n")
+                fo.write(f"{diff:.1f}\n")
             break
         elif (ctime >= start) and (ctime <= stop):
             diff = stop - ctime
-            ltime = 'End of Comm In: ' + convert_to_hour(diff)
+            hour, remainder = divmod(int(diff),3600)
+            minute = minute = remainder//60
+            ltime = f"End of Comm In:  {hour:>02}:{minute:>02}"
             with open(f"{HOUSE_KEEPING}/stime_to_comm", 'w') as fo:
                 fo.write(f"{0.0}\n")
             break
@@ -60,37 +64,20 @@ def find_next_comm():
 
     with open(f"{OUT_HTML_DIR}/ctest.xml", 'w') as fo:
         fo.write(f"<ncomm>\n{ltime}\n</ncomm>\n")
-    
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
 
-def convert_to_hour(stime):
-
-    hour = int(stime /3600)
-    diff = stime - hour * 3600
-    mm   = int(diff/60)
-
-    ltime = adjust_digit(hour) + ':' + adjust_digit(mm)
-
-    return ltime
-    
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-
-def adjust_digit(val):
-
-    val  = int(val)
-    sval = str(val)
-    if val < 10:
-        sval = '0' + sval
-
-    return sval
-    
-    
 #-------------------------------------------------------------------
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", choices = ['flight','test'], required = True, help = "Determine running mode.")
+    parser.add_argument("-p", "--path", required = False, help = "Directory path to determine output location of files.")
+    args = parser.parse_args()
 
-    find_next_comm()
+    if args.mode == "test":
+        OUT_HTML_DIR = f"{os.getcwd()}/test/outTest"
+        HOUSE_KEEPING = OUT_HTML_DIR
+        os.makedirs(OUT_HTML_DIR, exist_ok = True)
+        find_next_comm()
+
+    elif args.mode == "flight":
+        find_next_comm()
