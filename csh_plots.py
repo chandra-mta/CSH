52d797b78c4b48496f3f08fe9207b9eb3367b0fd
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-01-02 13:02:59 -0500

    Initial Commit.

---
+++ csh_plots.py
@@ -0,0 +1,15 @@
+#!/proj/sot/ska3/flight/bin/python
+
+"""
+**csh_plots.py**: Use the msid_plotting package to generate recent plots of maude CSH MSID's.
+
+:Author: W. Aaron (william.aaron@cfa.harvad.edu)
+:Last Updated: Jan 02, 2026
+
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "msid_plotting>=0.2",
+# ]
+# ///
+"""
