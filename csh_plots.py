2b20e3284f1e55a2268723dbf67d0053c28b920f
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-01-06 14:33:41 -0500

    Format main script run into a comm check verification method with additional annotations in the subpage.

--- csh_plots.py
+++ csh_plots.py
@@ -20,7 +20,9 @@ import argparse
 from typing import Dict, Any
 from datetime import timedelta
 from cxotime import CxoTime
+
 import msid_plotting
+from msid_plotting import comm_check
 
 #
 #--- Define Directory Pathing
@@ -31,7 +33,7 @@ HOUSE_KEEPING = f"{BIN_DIR}/house_keeping"
 PLOT_CONFIG_FILE = f"{HOUSE_KEEPING}/plot_configurations.json"
 NOW = CxoTime()
 BIN_SIZE = 500
-
+RUN = False #: Override the comm check and run regardless
 _NCOLS = {
     1:1,
     2:2,
@@ -52,31 +54,59 @@ def get_options(args=None):
     return opt
 
 def main():
+    """
+    Check if we are in comm and plot recent maude data.
+    """
+    #: Pull DSN Comm Information
+    comm_info = comm_check.comm_check()
+    comm_time_info = comm_check.translate(comm_info['comm'])
+    state_info = comm_check.in_state(**comm_time_info)
 
-    with open(PLOT_CONFIG_FILE) as f:
-        plot_configurations = json.load(f)
-    
-    stop = NOW
-    start = NOW - timedelta(days=3)
+    #: Format the DSN Comm information as a title annotation to plots
+    if state_info['in_track']:
+        #: Updating the data in plot right now
+        run = True
+        comm_annotation = f"Last Updated: {NOW.date}\n"
+        comm_annotation += f"Current Comm Ends: {comm_time_info['track_stop'].date}"
+    elif (NOW - timedelta(minutes=5) < comm_time_info['track_stop'].date):
+        #: Just finished comm. Run once more and format the annotation for the next comm
+        run = True
+        comm_annotation = f"Last Updated: {comm_time_info['track_stop'].date}\n"
+        _time_info = comm_check.translate(comm_info['dsn_query'][1])
+        comm_annotation += f"Next DSN Comm: {_time_info['track_start'].date}"
+    else:
+        run = False
+        _time_info = comm_check.translate(comm_info['previous_comm'])
+        comm_annotation = f"Last Updated: {_time_info['track_stop'].date}\n"
+        comm_annotation += f"Next DSN Comm: {comm_time_info['track_start'].date}"
 
-    #: Add Template
-    msid_plotting.msid_plot.JINJA_TEMPLATE_ENV.add_template_directory(
-        f"{os.path.dirname(__file__)}/template"
-    )
+    if run or RUN:        
+        with open(PLOT_CONFIG_FILE) as f:
+            plot_configurations = json.load(f)
+        
+        stop = NOW
+        start = NOW - timedelta(days=2)
 
-    for category, config in plot_configurations.items():
-        msid_ls = config.get('msid_ls')
-        title = config.get('title')
-        units = config.get('units')
-        weight = config.get('weight')
+        #: Add Template Directory
+        msid_plotting.msid_plot.JINJA_TEMPLATE_ENV.add_template_directory(
+            f"{os.path.dirname(__file__)}/template"
+        )
 
-        template_variables = {
-            'title': category,
-            }
-        generate_plot(category, msid_ls, start, stop, title, units, weight, template_variables)
+        for category, config in plot_configurations.items():
+            msid_ls = config.get('msid_ls')
+            title = config.get('title')
+            units = config.get('units')
+            weight = config.get('weight')
 
-def generate_plot(category, msid_ls, start, stop, title = None, units = None, weight = None, template_variables = {}):
+            template_variables = {
+                'title': f"Plot: {category}",
+                }
+            generate_plot(category, msid_ls, start, stop, comm_annotation, title, units, weight, template_variables)
 
+def generate_plot(category, msid_ls, start, stop, comm_annotation = None, title = None, units = None, weight = None, template_variables = {}):
+    """
+    Generate the plot html subpage for a given MSID set and associated parameters.
+    """
     multivar_plot = msid_plotting.msid_plot.MSIDPlot(
         msids = msid_ls,
         start = start,
@@ -87,6 +117,8 @@ def generate_plot(category, msid_ls, start, stop, title = None, units = None, we
     params : Dict[str, Any] = {'size': 5}
     if title is not None:
         params['title'] = title
+        if comm_annotation is not None:
+            params['title'] += f"\n{comm_annotation}"
     if units is not None:
         params['y_axis_labels'] = [f"{msid}({unit})" for msid, unit in zip(multivar_plot.msids, units)]
     if weight is not None:
@@ -112,6 +144,7 @@ if __name__ == "__main__":
         HOUSE_KEEPING = f"{os.getcwd()}/house_keeping"
         PLOT_DIR = f"{os.getcwd()}/test/_outTest"
         os.makedirs(PLOT_DIR, exist_ok = True)
+        RUN = True
     if opt.config is not None:
         PLOT_CONFIG_FILE = opt.config
     main()
