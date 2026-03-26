57fe217c0a1ff12382fb4f7c670f821cdfa5456e
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-03-26 14:18:25 -0400

    Incorporate the minutely comm check into the minutely telemetry fetch shell script.

--- fetch_telem.sh
+++ fetch_telem.sh
@@ -7,7 +7,8 @@ sleep 17
 python fetch_telemetry.py -m flight
 sleep 17
 python fetch_telemetry.py -m flight
-sleep 14
+
+python next_comm_check.py -m flight
 #: Allow three seconds for file copying of blobs and comparison values to occur
 cp -f ${SOH_WEB_DIR}/blob_*.json ${SOH_WEB_DIR}/Backup/
 cp -f ${SOH_DIR}/house_keeping/comp_limit_values.json ${SOH_DIR}/house_keeping/comp_limit_values.json~
